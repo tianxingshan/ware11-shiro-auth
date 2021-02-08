c527aaee03e6ac4c865945095d3c116b391636fb
“sunwansha” <qqew@qq.com>
2021-02-08 10:31:17 +0800

    注解式授权改为编程式授权

--- src/main/java/com/boot/shiro/controller/ShiroController.java
+++ src/main/java/com/boot/shiro/controller/ShiroController.java
@@ -1,8 +1,15 @@
 package com.boot.shiro.controller;
 
+import com.boot.shiro.entity.SysUserEntity;
 import com.boot.shiro.mapper.SysMenuMapper;
 import com.boot.shiro.util.ShiroUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -59,6 +67,18 @@ public class ShiroController {
         //UserRealm继承AuthorizingRealm，重写doGetAuthenticationInfo,查看数据库验证
         subject.login(token);
         LOGGER.info("登录成功");
+
+
+     /*   ObjectMapper mapper = new ObjectMapper();
+        SysUserEntity sysUserEntity = new SysUserEntity();
+        sysUserEntity.setUsername("张三丰");
+        String s = "";
+        try {
+            s = mapper.writeValueAsString(sysUserEntity);
+            System.out.println("json字符串："+s);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }*/
         return "登录成功";
     }
 
@@ -68,8 +88,16 @@ public class ShiroController {
      * 权限要求：sys:user:shiro
      */
     @RequestMapping("/menu/list")
-    @RequiresPermissions("sys:user:shiro")
+//    @RequiresPermissions("sys:user:shiro")
     public List list(ShiroHttpServletRequest request,ShiroHttpServletRequest req){
+        Subject subject = ShiroUtils.getSubject();
+        boolean authenticated = subject.isAuthenticated();
+        if(!authenticated){
+            //如果没授权，则抛出未授权异常
+            throw new AuthorizationException();
+        }
+        System.out.println("授权成功");
+
 
         return sysMenuMapper.selectList() ;
     }
@@ -79,7 +107,7 @@ public class ShiroController {
      * 权限要求：ccc:ddd:bbb
      */
     @RequestMapping("/menu/list2")
-    @RequiresPermissions("ccc:ddd:bbb")
+//    @RequiresPermissions("ccc:ddd:bbb")
     public List list2(HttpServletRequest request){
 
         return sysMenuMapper.selectList() ;
@@ -92,5 +120,45 @@ public class ShiroController {
     public String logout (){
         ShiroUtils.logout();
         return "success" ;
+    }
+
+    public static void main(String[] args) {
+//        jackson测试
+//对象转json字符串
+        ObjectMapper mapper = new ObjectMapper();
+        SysUserEntity sysUserEntity = new SysUserEntity();
+        sysUserEntity.setUsername("张三丰");
+        String s = "";
+        String s1;
+        try {
+              s = mapper.writeValueAsString(sysUserEntity);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject jsonObject = JSONObject.fromObject(sysUserEntity);
+        s1 = jsonObject.toString();
+        System.out.println("jackson对象转json(字符串)：" + s );
+        System.out.println("jsob-lib对象转json:"+s1);
+
+        List<SysUserEntity> list = new ArrayList<SysUserEntity>();
+        SysUserEntity u1= new SysUserEntity();
+        u1.setUsername("张无忌");
+        SysUserEntity u2 = new SysUserEntity();
+        u2.setUsername("张翠山");
+        list.add(u1);
+        list.add(u2);
+        try {
+            String list2Json = mapper.writeValueAsString(list);
+            System.out.println("Jackson集合list转json：" + list2Json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+//        JSONObject jsonObject1 = JSONObject.fromObject(list);
+        JSONArray jsonArray = JSONArray.fromObject(list);
+        String s2 = jsonArray.toString();
+        System.out.println("JSON-lib集合list转json:" + s2);
     }
 }
